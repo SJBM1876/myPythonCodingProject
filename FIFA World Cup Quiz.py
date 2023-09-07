72c7d65ef92763cbb1a44106f50914679ee662ff
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2023-09-07 14:53:14 +0100

    Add files via upload

---
+++ FIFA World Cup Quiz.py
@@ -0,0 +1,32 @@
+print("Welcome to the FIFA World Cup Quiz.")
+print("Test your knowledge on the FIFA Word Cup from the inaugral 1930 world cup in Uruguay to the 2022 world cup in Qatar.")
+playing = input("Are you ready to play?: ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Let's kick off")
+score = 0
+
+answer = input("Who won the very first world cup in 1930? ")
+if answer == "Uruguay":
+    print("GOAL!")
+    score += 1
+else:
+    print("SAVED!")
+
+
+answer = input("Who were the first European to win the world cup? ")
+if answer == "Italy":
+    print("GOAL!")
+    score += 1
+else:
+    print("SAVED!")
+
+print("Congratulations! You got " + str(score) + " questions correct.")
+
+
+
+
+          
+
