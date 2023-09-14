c05f44e631d867c7bf7ddcf3ec73b8b41ca18206
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2023-09-14 10:22:32 +0100

    Add files via upload
    Added an input for answers.

--- Timed maths challenge.py
+++ Timed maths challenge.py
@@ -3,6 +3,7 @@ import random
 OPERATORS = ["+", "*"]
 MIN_OPERAND = 2
 MAX_OPERAND = 10
+TOTAL_QUESTIONS = 5
 
 def create_question():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -10,9 +11,13 @@ def create_question():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-create_question()
+for i in range(TOTAL_QUESTIONS):
+    expr, answer = create_question()
+    guess = input("Question #" + str(i + 1) + ": " + expr + " = ")
+    print(guess)
+
 
 
